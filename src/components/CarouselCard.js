f381b4324499582bac9095274cd0235621d86b8b
Thuy Pham Thu <twee64@Thuys-MacBook-Air.local>
2024-06-04 13:30:07 +0800

    clean file

--- src/components/CarouselCard.js
+++
@@ -1,8 +0,0 @@
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-function CarouselCard() {
-  return <div></div>;
-}
-
-export default CarouselCard;
